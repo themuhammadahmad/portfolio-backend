689f762ea5959cf949101af6a18ebf5bb27f3c54
ahmadAK07 <muhammadahmadamin512@gmail.com>
2025-05-20 22:30:10 +0500

    updated

--- controllers/contactControllers.js
+++ controllers/contactControllers.js
@@ -1,7 +1,3 @@
-// const {getPrevData, addContactData} = require("../utils/functions.js");
-// const paths = require("../config/paths.js");
-// const dataFilePath = paths.dataFile;
-// let prevData = getPrevData(dataFilePath);
 const db = require("../firebase/firebase.js");
 async function createContact(req, res) {
   const { name, email, message } = req.body;
@@ -12,38 +8,7 @@ async function createContact(req, res) {
       message: "All fields are required",
       success: false
     });
-  }
-
-  // const ExistAlready = prevData.find(item => item.email === email);
-
-  // 409 Conflict – duplicate email
-  // if (ExistAlready) {
-  //   return res.status(409).json({
-  //     message: "Email already exists",
-  //     success: false
-  //   });
-  // }
-
-  // try {
-  //   addContactData(dataFilePath, { name, email, message });
-
-  //   // 201 Created – success
-  //   return res.status(201).json({
-  //     message: "Submitted Successfully",
-  //     success: true
-  //   });
-  // } catch (error) {
-  //   console.error(error);
-
-  //   // 500 Internal Server Error – catch block
-  //   return res.status(500).json({
-  //     message: "Something went wrong",
-  //     success: false
-  //   });
-  // }
-
-
-  
+  }  
 
    try {
         const existingUser = await db
@@ -53,13 +18,13 @@ async function createContact(req, res) {
       .get();
 
     if (!existingUser.empty) {
-      return res.status(400).json({ error: 'Email already exists' });
+      return res.status(400).json({ error: 'Email already exists', success: false });
     }
     
     const docRef = await db.collection('users').add({ name, email, message });
-    res.status(201).json({ message: 'User added', id: docRef.id });
+    res.status(201).json({ message: 'Submited Successfully', id: docRef.id, success: true });
   } catch (error) {
-    res.status(500).json({ error: 'Error adding user' });
+    res.status(500).json({ error: 'Error adding user', success: false });
   }
 }
 
